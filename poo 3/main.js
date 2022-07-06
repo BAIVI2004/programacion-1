0c15df095f6c59cfe732a44b9afa1e9e83a9a42b
BAIVI2004 <braianvidal777@gmail.com>
2022-07-06 14:19:35 -0300

    poo3

---
+++ poo 3/main.js
@@ -0,0 +1,23 @@
+//dejo configurado el export
+import Producto from "./producto.js";
+
+
+function guardar()
+{
+    let descripcion=document.getElementById("descripcion").value
+    let precio=document.getElementById("precio").value
+    let categoria=document.getElementById("categoria").value
+
+    let producto=new Producto(descripcion,precio,categoria)
+
+    //invocamos(ejecutamos) el metodo guardar_producto, perteneciente a la xlase prdocuto
+    producto.guardar_producto()
+}
+document.getElementById("btn_guardar").addEventListener('click',guardar)
+
+function listar(){
+    let producto=new Producto()
+    producto.obtener_productos()
+}
+listar()
+
---
+++ poo 3/producto.js
@@ -0,0 +1,68 @@
+//dejo configurado el export
+export default class Producto{
+   //creo constructor
+   constructor(d,p,c){
+       this.descripcion=d
+       this.precio=p
+       this.categoria=c
+   }
+
+   //creo metodo 
+   guardar_producto(){
+      let nuevo_producto={
+          descripcion:this.descripcion,
+          precio:this.precio,
+          categoria:this.categoria
+      }
+      if("Productos" in localStorage){
+          //convertimos el JSON(que fuimos a buscar al storag) en una expresion nativa en javascript
+           let lista_productos=JSON.parse( localStorage.getItem("Productos"))
+           lista_productos.push(nuevo_producto)
+           localStorage.setItem("Productos",JSON.stringify(lista_productos))
+      }else{
+           let lista_productos=[]
+           lista_productos.push(nuevo_producto)
+
+           /*
+           -guardar en localStorage
+           -el setitem crea un item(si es q no hay)
+           -el json.stringify es para pasar los datos a json
+           */
+
+           localStorage.setItem("Productos", JSON.stringify(lista_productos))
+       }
+       //invoco nuevamente el metodo obtener_productos()para
+       //lograr reconstruir la tabla y asi reflejar los datos actualizados
+       //que proviene  del localstorage,especificamente del item "productos"
+       this.obtener_productos()
+   }
+
+   obtener_productos(){
+       /*
+       Recolectamos todos los productos alojados en el item "productos"
+       que estan en notacion JSON. Debemos convertirlo en una expresion
+       nativa de Javascript (es decir, hacemos un JSON.pasrse())
+       */
+      let lista_productos= JSON.parse(localStorage.getItem("Productos"))
+
+      let filas = []
+      lista_productos.forEach( (element,index) => {
+         let fila=`
+         <tr>
+            <td>${index+1}</td>
+            <td>${element.descripcion}</td>
+            <td>${element.precio}</td>
+            <td>${element.categoria}</td>
+            <td>
+               <button class="btn btn-danger btn-sm">
+                  <i class="fa fa-trash"></i>
+               </button>
+            </td>
+          
+         </tr>`
+
+         filas.push(fila)
+      });
+      document.getElementById("tbody").innerHTML=filas.join('')
+   }
+}
